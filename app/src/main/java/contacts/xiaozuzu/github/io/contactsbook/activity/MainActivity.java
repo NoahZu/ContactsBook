5d45ee5f21cde8aec1cc3a5018b90cbf877e0a48
smallzoo <1010138814@qq.com>
2016-01-14 14:48:16 +0800

    美化拨号界面

--- app/src/main/java/contacts/xiaozuzu/github/io/contactsbook/activity/MainActivity.java
+++ app/src/main/java/contacts/xiaozuzu/github/io/contactsbook/activity/MainActivity.java
@@ -101,6 +101,26 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         tabLayout.setupWithViewPager(viewPager);
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (position == 0){
+                    addContact.setVisibility(View.INVISIBLE);
+                }else {
+                    addContact.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
     }
 
     public List<Contact> getContacts(){
